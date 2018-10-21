4ff007fdd9487abb30aa1948859ea3eb63be7935
Chu Hean <chuhean@gmail.com>
2018-10-21 11:44:47 +0000

    add checkRobotMovement function in new file

---
+++ checkRobotMovement.js
@@ -0,0 +1,27 @@
+//This function will check whether the robot movement and direction is valid. If not, it will undo the movement and show error to user.
+//It also check the robot direction. If the direction exceed/below the number listed in the 'direction' object, it will change it to appropriate number.
+
+module.exports = {
+  checkRobotMovement: function(x, y, f) {
+      
+    if (x > 5){
+        x = 5;
+        console.log('Invalid movement. Robot will fall down the table.');
+    } else if (x < 0){
+        x = 0;
+        console.log('Invalid movement. Robot will fall down the table.');
+    } else if (y > 5){
+        y = 5;
+        console.log('Invalid movement. Robot will fall down the table.');
+    } else if (y < 0){
+        y = 0;
+        console.log('Invalid movement. Robot will fall down the table.');
+    } else if (f > 4){
+        f = 1;
+    } else if (f < 1){
+        f = 4;
+    }
+
+  }
+
+};
