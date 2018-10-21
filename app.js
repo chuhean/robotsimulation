ce7c786e82c5a8dcb14980491391fe4c7893c3ea
Chu Hean <chuhean@gmail.com>
2018-10-21 14:16:18 +0000

    separate two questions into three questions function for improved logic flow

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const checkRobotMovement = require('./checkRobotMovement');
 
+//Declare robot X, Y, and Direction variables.
 let robotXCoordinate;
 let robotYCoordinate;
 let robotDirection;
@@ -21,7 +22,7 @@ function getKeyByValue(object, value) {
 
 
 //*************************************************Questions Prompts******************************************************
-const initialQuestions = [
+const initialQuestion = [
     { type: 'input', 
     name: 'confirmPlacement', 
     message: 'Please type "PLACE" to start',
@@ -41,10 +42,15 @@ const initialQuestions = [
         }, 0);
       }
     },
-    { type: 'input', name: 'confirmCoor', message: 'Type the placement coordinate in X,Y,F format.'}
 ];
 
-const questions = [{
+const secondQuestion = [{
+  type: 'input',
+  name: 'response',
+  message: 'Type the placement coordinate in X,Y,F format.',
+}];
+
+const thirdQuestions = [{
   type: 'input',
   name: 'response',
   message: 'What do you want to do: PLACE/MOVE/LEFT/RIGHT/REPORT ?',
@@ -53,83 +59,120 @@ const questions = [{
 
 
 //********************************************Command Line Questions & Response********************************************
-inquirer
-    .prompt(initialQuestions)
-    .then(answers => {
+//'question1' will prompt user to type 'PLACE' 
+const question1 = () => {
+    inquirer
+        .prompt(initialQuestion)
+        .then(answers => {
+            question2();
+        });
+
+};
+
+//'question2' prompt user for robot X,Y,F coordinate. Then user will be directed to 'question3' function.
+const question2 = () => {
+    inquirer
+        .prompt(secondQuestion)
+        .then(answers => {
+            let placementCoor = answers.confirmCoor.split(',');
+            robotXCoordinate = Number(placementCoor[0].trim());
+            robotYCoordinate = Number(placementCoor[1].trim());
+            let robotTextDirection = String(placementCoor[2].toUpperCase().trim());
+            
+            //Check if input coordinates is one of the following number. Otherwise show error to user.
+            if (![0,1,2,3,4,5].includes(robotXCoordinate) || ![0,1,2,3,4,5].includes(robotYCoordinate)){
+                console.log('Invalid coordinate input. Coordinate must be between 0 to 5.');
+                question1();
+            } 
+            
+            //Check if input direction is one of the following direction. Otherwise show error to user.
+            else if (!["NORTH", "EAST", "SOUTH", "WEST"].includes(robotTextDirection)){
+                console.log('Invalid direction input. Direction must be either NORTH, EAST, SOUTH, or WEST.');
+                question1();
+            }
+            
+            //Assign numerial direction according to 'direction' object.
+            robotDirection = direction[robotTextDirection];
+            
+            question3();
         
-        let placementCoor = answers.confirmCoor.split(',');
-        robotXCoordinate = Number(placementCoor[0].trim());
-        robotYCoordinate = Number(placementCoor[1].trim());
-        let robotTextDirection = String(placementCoor[2].toUpperCase().trim());
-        
-        //Check if input coordinates is one of the following number. Otherwise show error to user.
-        if (![0,1,2,3,4,5].includes(robotXCoordinate) || ![0,1,2,3,4,5].includes(robotYCoordinate)){
-            console.log('Invalid coordinate input. Coordinate must be between 0 to 5.');
-        } 
-        //Check if input direction is one of the following direction. Otherwise show error to user.
-        else if (!["NORTH", "EAST", "SOUTH", "WEST"].includes(robotTextDirection)){
-            console.log('Invalid direction input. Direction must be either NORTH, EAST, SOUTH, or WEST.');
-        }
-        
-        //Assign numerial direction according to 'direction' object.
-        robotDirection = direction[robotTextDirection];
-        
-        inquirer
-            .prompt(questions)
-            .then(answers => {
-                //Sanitize user's input to upper case and remove all whitespace.
-                let sanitizedAnswer = answers.response.toUpperCase().trim();
+        });
+    
+};
+
+//'question2' will prompt user for the following command: PLACE, MOVE, LEFT, RIGHT, REPORT. 
+const question3 = () => {
+    inquirer
+        .prompt(thirdQuestions)
+        .then(answers => {
+            //Sanitize user's input to upper case and remove all whitespace.
+            let sanitizedAnswer = answers.response.toUpperCase().trim();
+            
+            //Run this code block if user types 'MOVE'. Then add or minus their coordinate according to their direction. 
+            if (sanitizedAnswer === 'MOVE'){
                 
-                //Run this code block if user types 'MOVE'. Then add or minus their coordinate according to their direction. 
-                if (sanitizedAnswer === 'MOVE'){
-                    
-                    if (robotDirection === 1){
-                        robotXCoordinate++;
-                    } else if (robotDirection === 2){
-                        robotYCoordinate++;
-                    } else if (robotDirection === 3){
-                        robotXCoordinate--;
-                    } else {
-                        robotYCoordinate--;
-                    }
-                    
-                    //Check if robot will fall down or not. If robot falls down, undo the previous movement, and show an error to user.
-                    checkRobotMovement.checkXY(robotXCoordinate, robotYCoordinate);
-                    
+                if (robotDirection === 1){
+                    robotXCoordinate++;
+                } else if (robotDirection === 2){
+                    robotYCoordinate++;
+                } else if (robotDirection === 3){
+                    robotXCoordinate--;
+                } else {
+                    robotYCoordinate--;
                 }
                 
-                //Run this code block if user types 'LEFT'. Then move the robot direction's to left side, by minus 1 to their numerical direction. 
-                else if (sanitizedAnswer === 'LEFT'){
-                    
-                    robotDirection--;
-                    
-                    //Check if the direction exceed/below the number listed in the 'direction' object, and change it to appropriate number.
-                    checkRobotMovement.checkDirection(robotDirection);
-                    
-                }
+                //Check if robot will fall down or not. If robot falls down, undo the previous movement, and show an error to user.
+                checkRobotMovement.checkXY(robotXCoordinate, robotYCoordinate);
                 
-                //Run this code block if user types 'RIGHT'. Then move the robot direction's to right side, by adding 1 to their numerical direction. 
-                else if (sanitizedAnswer === 'RIGHT'){
-                    
-                    robotDirection++;
-                    
-                    //Check if the direction exceed/below the number listed in the 'direction' object, and change it to appropriate number.
-                    checkRobotMovement.checkDirection(robotDirection);
-                    
-                }
+                question3();
+            }
+            
+            //Run this code block if user types 'LEFT'. Then move the robot direction's to left side, by minus 1 to their numerical direction. 
+            else if (sanitizedAnswer === 'LEFT'){
                 
-                //Run this code block if user types 'REPORT'. Then report back the robot coordinate in the console. 
-                else if (sanitizedAnswer === 'REPORT'){
-                    
-                    let robotTextDirection = getKeyByValue(direction, robotDirection);
-                    console.log(`The robot's coordinate is (${robotXCoordinate}, ${robotYCoordinate}, ${robotTextDirection}).`);
-                    
-                }
+                robotDirection--;
                 
-                //Check if sanitized answer satisfy one of the answer options above. Otherwise show an error to user.
-                else {
-                    console.log('Invalid input. Please try again.');
-                }
-            });
+                //Check if the direction exceed/below the number listed in the 'direction' object, and change it to appropriate number.
+                checkRobotMovement.checkDirection(robotDirection);
+                
+                question3();
+            }
+            
+            //Run this code block if user types 'RIGHT'. Then move the robot direction's to right side, by adding 1 to their numerical direction. 
+            else if (sanitizedAnswer === 'RIGHT'){
+                
+                robotDirection++;
+                
+                //Check if the direction exceed/below the number listed in the 'direction' object, and change it to appropriate number.
+                checkRobotMovement.checkDirection(robotDirection);
+                
+                question3();
+            }
+            
+            //Run this code block if user types 'REPORT'. Then report back the robot coordinate in the console. 
+            else if (sanitizedAnswer === 'REPORT'){
+                
+                let robotTextDirection = getKeyByValue(direction, robotDirection);
+                console.log(`The robot's coordinate is (${robotXCoordinate}, ${robotYCoordinate}, ${robotTextDirection}).`);
+                
+                question3();
+                
+            }
+            
+            //Run this code block if user types 'PLACE'. The application will start again from the beginning. 
+            else if (sanitizedAnswer === 'PLACE'){
+                
+                question2();
+                
+            }
+            
+            //Check if sanitized answer satisfy one of the answer options above. Otherwise show an error to user.
+            else {
+                console.log('Invalid input. Please try again.');
+                question3();
+            }
+        });
         
-    });
+};
+
+question1();
