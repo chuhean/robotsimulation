2c6596a4ebedc0b3b4f8e591400b10ca854ac3df
Chu Hean <chuhean@gmail.com>
2018-10-21 11:57:55 +0000

    change checkRobotMovement module and add its functions to each logic code block

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const checkRobotMovement = require('./checkRobotMovement');
 
 let robotXCoordinate;
 let robotYCoordinate;
@@ -17,7 +18,9 @@ function getKeyByValue(object, value) {
   return Object.keys(object).find(key => object[key] === value);
 }
 
-//**************************************Questions Prompts*****************************************
+
+
+//*************************************************Questions Prompts******************************************************
 const initialQuestions = [
     { type: 'input', 
     name: 'confirmPlacement', 
@@ -30,7 +33,7 @@ const initialQuestions = [
         setTimeout(function() {
           if (input.toUpperCase().trim() !== 'PLACE') {
             // Pass the return value in the done callback
-            done('You need to type "place" to start');
+            done('You need to type "PLACE" to start.');
             return;
           }
           // Pass the return value in the done callback
@@ -47,7 +50,9 @@ const questions = [{
   message: 'What do you want to do (PLACE/MOVE/LEFT/RIGHT/REPORT)?',
 }];
 
-//********************************Command Line Questions & Response******************************************
+
+
+//********************************************Command Line Questions & Response********************************************
 inquirer
     .prompt(initialQuestions)
     .then(answers => {
@@ -60,7 +65,7 @@ inquirer
     .prompt(questions)
     .then(answers => {
         //Sanitize user's input to lower case and remove all whitespace.
-        let sanitizedAnswer = answers.response.toUpperCase();
+        let sanitizedAnswer = answers.response.toUpperCase().trim();
         
         //Check if sanitized answer satisfy one of the answer options. Otherwise show an error to user.
         
@@ -77,25 +82,37 @@ inquirer
                 robotYCoordinate--;
             }
             
+            //Check if robot will fall down or not. If robot falls down, undo the previous movement, and show an error to user.
+            checkRobotMovement.checkXY(robotXCoordinate, robotYCoordinate);
+            
         }
         
         //Run this code block if user types 'LEFT'. Then move the robot direction's to left side, by minus 1 to their numerical direction. 
         if (sanitizedAnswer === 'LEFT'){
+            
             robotDirection--;
+            
+            //Check if the direction exceed/below the number listed in the 'direction' object, and change it to appropriate number.
+            checkRobotMovement.checkDirection(robotDirection);
+            
         }
         
         //Run this code block if user types 'RIGHT'. Then move the robot direction's to right side, by adding 1 to their numerical direction. 
         if (sanitizedAnswer === 'RIGHT'){
+            
             robotDirection++;
+            
+            //Check if the direction exceed/below the number listed in the 'direction' object, and change it to appropriate number.
+            checkRobotMovement.checkDirection(robotDirection);
+            
         }
         
         //Run this code block if user types 'REPORT'. Then report back the robot coordinate in the console. 
         if (sanitizedAnswer === 'REPORT'){
+            
             let robotTextDirection = getKeyByValue(direction, robotDirection);
             console.log(`The robot's coordinate is (${robotXCoordinate}, ${robotYCoordinate}, ${robotTextDirection}).`);
+            
         }
-        
-        //Check if robot will fall down or not. If robot falls down, undo the previous movement, and show an error to user.
-        checkRobotMovement();
         
     });
